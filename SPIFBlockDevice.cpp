8e48f1ae039ce11dde70363e3546dac846ee6e78
Offir Kochalsky <offir.kochalsky@arm.com>
2018-08-28 14:02:26 +0300

    Fixed offset not increased correctly at unaligned erase

--- SPIFBlockDevice.cpp
+++ SPIFBlockDevice.cpp
@@ -333,6 +333,7 @@ int SPIFBlockDevice::erase(bd_addr_t addr, bd_size_t in_size)
     }
 
     int type = 0;
+    uint32_t offset = 0;
     uint32_t chunk = 4096;
     int cur_erase_inst = _erase_instruction;
     int size = (int)in_size;
@@ -343,7 +344,7 @@ int SPIFBlockDevice::erase(bd_addr_t addr, bd_size_t in_size)
     // Erase Types of selected region
     uint8_t bitfield = _region_erase_types_bitfield[region];
 
-    tr_debug("DEBUG: erase - addr: %llu, in_size: %llu", addr, in_size);
+    tr_info("DEBUG: erase - addr: %llu, in_size: %llu", addr, in_size);
 
     // For each iteration erase the largest section supported by current region
     while (size > 0) {
@@ -352,7 +353,8 @@ int SPIFBlockDevice::erase(bd_addr_t addr, bd_size_t in_size)
         // find the matching instruction and erase size chunk for that type.
         type = _utils_iterate_next_largest_erase_type(bitfield, size, (unsigned int)addr, _region_high_boundary[region]);
         cur_erase_inst = _erase_type_inst_arr[type];
-        chunk = _erase_type_size_arr[type];
+        offset = addr % _erase_type_size_arr[type];
+        chunk = ( (offset + size) < _erase_type_size_arr[type]) ? size : (_erase_type_size_arr[type] - offset);
 
         tr_debug("DEBUG: erase - addr: %llu, size:%d, Inst: 0x%xh, chunk: %lu , ",
                  addr, size, cur_erase_inst, chunk);
